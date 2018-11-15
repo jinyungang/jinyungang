d0b290ba160d74567c64f67d2d64b2fd8fec5ffb
jinYunGang <jinyungang@youyuwo.com>
2018-11-15 18:14:39 +0800

    update HELLO WORLD

---
+++ src/test/java/javaTest.java
@@ -0,0 +1,9 @@
+/**
+ * @author JinYunGang
+ * @date 2018/11/15 18:13
+ */
+public class javaTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
